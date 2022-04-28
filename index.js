30116198f1faa30420c020416942c86f32858296
Ross <rossgarfield@gmail.com>
2022-04-28 16:26:07 -0500

    commit tree b307749785a3de63dd4505e5c76a0a04e251d84e

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+const destructivelyAppendCat = name => cats.push(name);
+
+const destructivelyPrependCat = name => cats.unshift(name);
+
+const destructivelyRemoveLastCat = name => cats.pop(name);
+
+const destructivelyRemoveFirstCat = name => cats.shift(name);
+
+const appendCat = (name) =>{
+    const copyCats = [...cats];
+    copyCats.push(name);
+    return copyCats;
+}
+
+const prependCat = (name) =>{
+    const copyCats = [...cats];
+    copyCats.unshift(name);
+    return copyCats;
+}
+
+function removeLastCat() {
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;
+}
+
+function removeFirstCat() {
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats;
+}
+
+
+
